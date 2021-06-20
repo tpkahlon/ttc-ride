163fd7706ee8b41d2c1acfb78e80d7ca9532d9ed
Tej <tkahlon@labx.com>
2021-06-20 16:42:55 -0400

    f

--- pages/index.js
+++ pages/index.js
@@ -89,7 +89,7 @@ export default function Home() {
           </footer>
         </main>
       ) : (
-        <main>
+        <main className="video">
           <ReactPlayer
             url={`./${station}-${direction}.mp4`}
             controls
